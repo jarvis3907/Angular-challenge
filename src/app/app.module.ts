d7056ca3e7c90b52b57d9606fb1034df180b7540
Chaitanya Sunkara <chaitanya.sunkara.ext@ing.com>
2021-02-02 21:25:13 +0100

    angular interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { AddTasksMainComponent } from './add-tasks-main/add-tasks-main.component
 import { CreateTaskComponent } from './create-task/create-task.component';
 import { TaskSummaryComponent } from './task-summary/task-summary.component';
 import { ExecuteTaskComponent } from './execute-task/execute-task.component';
+import { HeaderInterceptor } from './header.interceptor';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { ExecuteTaskComponent } from './execute-task/execute-task.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
